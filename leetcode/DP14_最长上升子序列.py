62306164489eb94f584bdd291fb6ee06b7c9dceb
tw-panda <798262113@qq.com>
2022-06-26 10:06:35 +0800

    update

--- leetcode/DP14_最长上升子序列.py
+++ leetcode/DP14_最长上升子序列.py
@@ -4,6 +4,10 @@ def lengthOfLIS(n, nums):
     dp = [1 for _ in range(n)]
     for i in range(1, n):
         for j in range(i):
+            """ 
+            计算以第i个为结尾的,最长子序列
+            x[0:j] + x[i]组成的串,最长就是 dp[j] 或者 dp[j] + 1，条件就行x[j] 和 x[i] 的大小
+            """
             if nums[j] < nums[i]:
                 dp[i] = max(dp[i], dp[j] + 1)
     return max(dp)
